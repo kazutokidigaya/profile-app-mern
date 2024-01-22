84d3da07073a6f2773aae45be2bdc01b0ee4ecb8
kazutokidigaya <2001karanpt@gmail.com>
2024-01-22 15:17:38 +0530

    added all the chnages

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 const authenticate = (req, res, next) => {
   const user = basicAuth(req);
-  console.log(ADMIN_USER, ADMIN_PASSWORD, user);
+
   if (!user || user.name !== ADMIN_USER || user.pass !== ADMIN_PASSWORD) {
     res.set("WWW-Authenticate", 'Basic realm="401"');
     res.status(401).send("Authentication required."); // custom message
